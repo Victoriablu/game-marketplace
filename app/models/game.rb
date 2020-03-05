5ffbf81b566b6c2f9fe441c7c80c6ad507ca16a6
Victoriablu <Victoriablu@users.noreply.github.com>
2020-03-05 15:43:47 +1100

    Added NAV bar

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,5 @@
 class Game < ApplicationRecord
-    validates :title, :platform, :description, presence: true,
-    length: { minimum: 5 }
-    validates :condition, :price, presence: true,
+    validates :title, :platform, :description,:condition, :price, presence: true,
     length: { minimum: 2 }
     
     belongs_to :user
